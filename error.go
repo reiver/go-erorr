7f535b822d07c81a82730afc0d67d90ccbbd6095
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-12-18 20:33:43 -0800

    docs

--- error.go
+++ error.go
@@ -5,6 +5,8 @@ package erorr
 // For example:
 //
 //	const ErrNilReceiver = erorr.Error("nil receiver")
+//
+// Use Error for your core errors.
 type Error string
 
 func (receiver Error) Error() string {
