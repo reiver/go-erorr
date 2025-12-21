5c9204c3d4ddd8ae5f331de86e7c55c511be35ce
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-12-21 11:58:51 -0800

    wrap

--- wrap.go
+++ wrap.go
@@ -4,7 +4,23 @@ import (
 	"codeberg.org/reiver/go-field"
 )
 
+// Wrap returns a wrapped annotated error, which includes a call-trace and fields.
+//
+// If `err` is nil then Wrap returns nil.
+//
+// Example usage:
+//
+//	err := something()
+//	if nil != err {
+//		return erorr.Wrap(err, "failed to do something",
+//			field.String("service", "banana"),
+//		)
+//	}
 func Wrap(err error, msg string, fields ...field.Field[string]) error {
+	if nil == err {
+		return nil
+	}
+
 	switch casted := err.(type) {
 	case Errors:
 		return WrapErrors(casted, msg, fields...)
