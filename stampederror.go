686955f679938a72d024fea7d4dea344d462005a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-12-21 11:45:26 -0800

    docs

--- stampederror.go
+++ stampederror.go
@@ -5,12 +5,24 @@ import (
 	"github.com/reiver/go-calltrace"
 )
 
+// StampedError is contextual error.
+//
+// When a StampedError is created includes a call-trace, and a list of fields.
+//
+// For example:
+//
+//	err := erorr.Stamp("client is nil",
+//		field.String("url", url),
+//	)
 type StampedError struct {
 	callTrace string
 	fields    []field.Field[string]
 	msg       string
 }
 
+// StampError returns a [StampedError].
+//
+// If you are not sure whetherto use [StampError] or [Stamp] use [Stamp].
 func StampError(msg string, fields ...field.Field[string]) StampedError {
 	callTrace := calltrace.String()
 
@@ -21,6 +33,9 @@ func StampError(msg string, fields ...field.Field[string]) StampedError {
 	}
 }
 
+// Stamp returns a [StampedError].
+//
+// If you are not sure whetherto use [StampError] or [Stamp] use [Stamp].
 func Stamp(msg string, fields ...field.Field[string]) error {
 	return StampError(msg, fields...)
 }
